b477175df63c7dc6a373093e5d6e13dceea5b206
kai271828 <40947038s@ntnu.edu.tw>
2023-08-01 13:29:06 +0800

    resume

--- finetune.py
+++ finetune.py
@@ -377,8 +377,8 @@ def main(
             lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
         ).__get__(model, type(model))
 
-    # if torch.__version__ >= "2" and sys.platform != "win32":
-    #     model = torch.compile(model)
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
 
     trainer.train()
 
