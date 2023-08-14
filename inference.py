13351c8ec45b5e02816db7895c6384452d331ba4
kai271828 <40947038s@ntnu.edu.tw>
2023-08-14 10:25:25 +0800

    modify inference method

--- inference.py
+++ inference.py
@@ -26,8 +26,6 @@ def main(
     bnb_4bit_compute_dtype: str = "float32",
     prompt_template_name: str = "instruction",
     cache_dir: str = "./cache",
-    instruction: str = "",
-    input: Union[str, None] = None,
     temperature: float = 0.1,
     top_p: float = 0.75,
     top_k: int = 10,
@@ -90,9 +88,6 @@ def main(
     if torch.__version__ >= "2" and sys.platform != "win32":
         model = torch.compile(model)
 
-    prompt = prompter.generate_prompt(instruction, input)
-    inputs = tokenizer(prompt, return_tensors="pt")
-    input_ids = inputs["input_ids"].to(device)
     generation_config = GenerationConfig(
         temperature=temperature,
         top_p=top_p,
@@ -100,18 +95,26 @@ def main(
         num_beams=num_beams,
     )
 
-    with torch.no_grad():
-        generation_output = model.generate(
-            input_ids=input_ids,
-            generation_config=generation_config,
-            return_dict_in_generate=True,
-            output_scores=True,
-            max_new_tokens=max_new_tokens,
-        )
-        s = generation_output.sequences[0]
-        print(s)
-        output = tokenizer.decode(s)
-        print(output)
+    while True:
+        instruction_ = input("Please enter instruction: ")
+        input_ = input("Please enter the corresponding input (or just skip): ")
+
+        prompt = prompter.generate_prompt(instruction_, input_)
+        inputs = tokenizer(prompt, return_tensors="pt")
+        input_ids = inputs["input_ids"].to(device)
+
+        with torch.no_grad():
+            generation_output = model.generate(
+                input_ids=input_ids,
+                generation_config=generation_config,
+                return_dict_in_generate=True,
+                output_scores=True,
+                max_new_tokens=max_new_tokens,
+            )
+            s = generation_output.sequences[0]
+            print(s)
+            output = tokenizer.decode(s)
+            print(output)
 
 
 if __name__ == "__main__":
