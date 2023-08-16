adff46203d36635c1e284b1318ff24fc206495fd
kai <40947038S@ntnu.edu.tw>
2023-08-16 15:38:50 +0800

    update

--- finetune.py
+++ finetune.py
@@ -381,7 +381,7 @@ def main(
         deepspeed=deepspeed,
     )
 
-    data_collator = transformers.DataCollatorWithPadding(tokenizer)
+    data_collator = transformers.DataCollatorWithPadding(tokenizer, padding=True)
 
     trainer = Trainer(
         model=model,
